ffde09203130b8889ee45fbfcd32adf3ed9d5d9e
harrymas21 <harrymas21@gmail.com>
2018-01-23 00:16:06 +0300

    dto objects added

---
+++ src/main/java/com/carbonara/karateboot/dtos/CommentDTO.java
@@ -0,0 +1,15 @@
+package com.carbonara.karateboot.dtos;
+
+public class CommentDTO{
+    private int blogId; 
+    private String body;
+    
+    // standard getters and setters
+    public int getBlogId() {return blogId;}
+
+  public void setBlogId(int blogId) {this.blogId = blogId;}
+  
+  public String getBody() {return body;}
+
+  public void setBody(String body) {this.body = body;}
+} 
---
+++ src/main/java/com/carbonara/karateboot/dtos/EventRegisterDTO.java
@@ -0,0 +1,15 @@
+package com.carbonara.karateboot.dtos;
+
+public class EventRegisterDTO{
+    private int eventId; 
+    private String category;
+    
+    // standard getters and setters
+  public int getEventId() {return eventId;}
+
+  public void setEventId(int eventId) {this.eventId = eventId;}
+  
+  public String getCategory() {return category;}
+
+  public void setCategory(String category) {this.category = category;}
+} 
