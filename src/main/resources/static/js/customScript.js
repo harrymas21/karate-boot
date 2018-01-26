8f82921a87527fcf7e96b3c277b06240084e84e6
harrymas21 <harrymas21@gmail.com>
2018-01-26 20:57:25 +0300

    daterange picker added

--- src/main/resources/static/js/customScript.js
+++ src/main/resources/static/js/customScript.js
@@ -1,11 +1,15 @@
- $(function () {
-     //logout function with ajax
-       $('#logout').on('click', function () {
-           $.ajax({
-                type: 'POST',
-                url: '/app-logout',
-                success: function (data) {}
+ $(function () {  
+       //check for empty
+       function isNotEmpty(val) {
+        return ((val !== '') && (val !== undefined) && (val.length > 0) && (val !== null));
+    }
+
+    
+  $('.jqueryui-marker-datepicker').datepicker({
+                dateFormat: "yy-mm-dd",
+                changeYear: true,
+                changeMonth: true,
+                text: false
             });
-       }); 
  });
 
